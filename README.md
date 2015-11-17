2328b10867f33a0c4ad78348cfdcd82b988a7d41
Vladislav Khvostov <khvostov@yamoney.ru>
2015-11-17 18:20:55 +0300

    clean up

--- README.md
+++ README.md
@@ -1,36 +1,32 @@
 # svg-fallback [![Build Status](https://secure.travis-ci.org/justim/postcss-svg-fallback.png)](https://travis-ci.org/justim/postcss-svg-fallback)
 
-> An automatic SVG converter for your CSS files, built on top of the [PostCSS] ecosystem.
+> This plugin  automatically writes CSS rule with fallback selector, built on top of the [PostCSS] ecosystem.
 
 ## Usage
 
-Right now it is only possible to use this as a [PostCSS] plugin:
+
+Here a way to add [PostCSS] plugin to [project-stub](https://github.com/bem/project-stub) configuration:
 
 ```js
-var postcss = require('postcss')
-var svgFallback = require('postcss-svg-fallback')
 
-var input = read(/* read some css */);
-postcss()
-	.use(svgFallback({
-		basePath: '', // base path for the images found in the css
-		dest: '', // destination for the generated SVGs
-		fallbackSelector: '.no-svg', // selector that gets prefixed to selector
-	})
-	.process(input)
-	.then(function(processor) {
-		var output = processor.toString();
-	});
+	// postCss
+	[require('enb-bundle-postcss/techs/enb-bundle-postcss'), {
+		source: '?.post.css',
+		sourcemap: true,
+		plugins: [require('postcss-svg-fallback')({
+			fallbackSelector: '.no-svg'
+		})]
+	}]
+
+
 ```
 
-> Note: we must use the async version of postcss
 
 Converts this:
 
 ```css
 .icon {
 	background: url(images/sun-is-shining.svg) no-repeat;
-	background-size: 20px 20px; /* background-size is mandatory */
 }
 ```
 
@@ -40,19 +36,12 @@ to this:
 .icon {
 	/* original declarations are untouched */
 	background: url(images/sun-is-shining.svg) no-repeat;
-	background-size: 20px 20px;
 }
 
 /* same selector, but with a prefix */
 .no-svg .icon {
-	/* a png image is generated and placed in the `dest` folder,
-	 * with default settings, that's right next to the original SVG
-	 */
-	background-image: url(images/sun-is-shining-20x20.png);
+	background-image: url(images/sun-is-shining.png);
 }
 ```
-
-Converting is done with [PhantomJS] and is only done for images that actually need conversion (`background-size` & `mtime`).
-
 [PostCSS]: https://github.com/postcss/postcss
-[PhantomJS]: http://phantomjs.org
+
--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 
 'use strict';
 
-var path = require('path');
-var fs = require('fs');
-
 var postcss = require('postcss');
-var async = require('async');
-var when = require('when');
-var phantomjs = require('phantomjs');
-var childProcess = require('child_process');
-
-var phantomjsScript = path.resolve(__dirname, './phantomjs-script.js');
 
 var backgroundImageRegex = /url\('?([^')]+\.svg)'?\)/;
-var backgroundSizeRegex = /^(\d+)px( (\d+)px)?$/;
+
 
 module.exports = postcss.plugin('postcss-svg-fallback', function(options) {
 	var fallbackSelector;
+
 	options = options || {};
 
 	fallbackSelector = options.fallbackSelector || '.no-svg';
 
+
 	return function (css, result) {
 		var images = [];
 
-		css.eachRule(function(rule) {
+		css.walkRules(function(rule) {
 			var backgroundImage;
-			var backgroundSize;
 			var newImage;
 			var newRule;
 			var newDecl;
@@ -37,9 +29,9 @@ module.exports = postcss.plugin('postcss-svg-fallback', function(options) {
 				return;
 			}
 
-			rule.eachDecl(function(decl) {
+			rule.walkDecls(function(decl) {
 				var backgroundImageMatch;
-				var backgroundSizeMatch;
+
 
 				if (decl.prop.match(/^background(-image)?$/)) {
 					backgroundImageMatch = backgroundImageRegex.exec(decl.value);
@@ -50,27 +42,16 @@ module.exports = postcss.plugin('postcss-svg-fallback', function(options) {
 					}
 				}
 
-				if (decl.prop === 'background-size') {
-					backgroundSizeMatch = backgroundSizeRegex.exec(decl.value);
 
-					if (backgroundSizeMatch) {
-						backgroundSize = {
-							width: parseInt(backgroundSizeMatch[1]),
-							height: parseInt(backgroundSizeMatch[3] || backgroundSizeMatch[1])
-						};
-					}
-				}
 			});
 
-			if (backgroundImage && backgroundSize) {
-				newImage = backgroundImage.replace(/\.svg$/, '-' + backgroundSize.width + 'x' + backgroundSize.height + '.png');
+			if (backgroundImage) {
+				newImage = backgroundImage.replace(/\.svg$/, '.png');
 
 				images.push({
 					postcssResult: result,
 					postcssRule: rule,
 					image: backgroundImage,
-					newImage: newImage,
-					size: backgroundSize
 				});
 
 				newRule = postcss.rule({ selector: fallbackSelector + ' ' + rule.selector });
@@ -78,7 +59,7 @@ module.exports = postcss.plugin('postcss-svg-fallback', function(options) {
 
 				newDecl = postcss.decl({
 					prop: 'background-image',
-					value: 'url(' + newImage + ')'
+					value: 'url(' + newImage + ')',
 				});
 				newDecl.source = matchedBackgroundImageDecl.source;
 
@@ -86,60 +67,5 @@ module.exports = postcss.plugin('postcss-svg-fallback', function(options) {
 				rule.parent.insertAfter(rule, newRule);
 			}
 		});
-
-		return when.promise(function(resolve, reject) {
-			async.eachSeries(images, processImage.bind(null, options), function(err) {
-				if (err) {
-					reject(err);
-				} else {
-					resolve();
-				}
-			});
-		});
 	};
 });
-
-function processImage(options, image, cb) {
-	var source = path.resolve(options.basePath || '', image.image);
-	var dest = path.resolve(options.dest || '', image.newImage);
-
-	var args = [
-		phantomjsScript,
-		source,
-		image.size.width,
-		image.size.height,
-		dest
-	];
-
-	fs.stat(source, function(err, sourceStat) {
-		if (sourceStat) {
-			fs.stat(dest, function(err, destStat) {
-				if (!destStat || sourceStat.mtime > destStat.mtime) {
-					runPhantomJs(args, cb);
-				} else {
-					cb();
-				}
-			});
-		} else {
-			image.postcssResult.warn(
-				'Could not find "' + image.image + '" at "' + source + '"',
-				{ node: image.postcssRule });
-
-			cb();
-		}
-	});
-}
-
-function runPhantomJs(args, cb) {
-	childProcess.execFile(phantomjs.path, args, function(err, stdout, stderr) {
-		if (err) {
-			cb(err);
-		} else if (stdout.length) {
-			cb(stdout.toString().trim());
-		} else if (stderr.length) {
-			cb(stderr.toString().trim());
-		} else {
-			cb();
-		}
-	});
-}
