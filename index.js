ef8ddb24a3cb485651a457858c78401ee4dfa30b
Vladislav Khvostov <vladkhvo@gmail.com>
2016-04-06 16:20:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,62 @@
+
 'use strict';
 var fs = require('fs');
 var path = require('path');
 var postcss = require('postcss');
+var backgroundImageRegex = /url\(['"]?(.+\.svg)['"]?\)/;
 
-var backgroundImageRegex = /url\('?([^')]+\.svg)'?\)/;
-
-module.exports = postcss.plugin('postcss-svg-to-png-selector', function(options) {
+module.exports = postcss.plugin('postcss-svg-to-png-selector', function (options) {
 	var fallbackSelector;
-
 	options = options || {};
-
 	fallbackSelector = options.fallbackSelector || '.no-svg';
 
 	return function (css, result) {
-		var images = [];
-
 		css.walkRules(function(rule) {
 			var backgroundImage;
-			var newImage;
+			var pngImage;
 			var newRule;
 			var newDecl;
 			var matchedBackgroundImageDecl;
-			var yamoneyProjectPathToNewImage;
+			var pathToSourceFile;
 
-			// skip our added rules
+			// Пропускаем добавленные правила.
 			if (rule.selector.indexOf(fallbackSelector) !== -1) {
 				return;
 			}
 
 			rule.walkDecls(function(decl) {
 				var backgroundImageMatch;
-
-
-				if (decl.prop.match(/^background(-image)?$/)) {
-					backgroundImageMatch = backgroundImageRegex.exec(decl.value);
-
+				if (decl.prop.match(/^background/)) {
+					backgroundImageMatch = decl.value.match(backgroundImageRegex);
+					pathToSourceFile = decl.source.input.file;
 					if (backgroundImageMatch) {
 						backgroundImage = backgroundImageMatch[1];
 						matchedBackgroundImageDecl = decl;
 					}
 				}
-
-
 			});
 
 			if (backgroundImage) {
-				newImage = backgroundImage.replace(/\.svg$/, '.png');
-
-				var pathToPng = path.dirname(css.source.input.file) + '/' + newImage;
-
+				pngImage = backgroundImage.replace(/\.svg/, '.png');
+				var pathToPng = path.resolve(path.dirname(pathToSourceFile), pngImage);
 				var checkExistPng = function() {
 					var exists;
 					try {
 						exists = fs.statSync(pathToPng).isFile();
 					} catch (err) {
 						exists = false;
-					}
-					return exists
+					};
+					return exists;
 				}
 
 				if (checkExistPng()) {
-
-					images.push({
-						postcssResult: result,
-						postcssRule: rule,
-						image: backgroundImage,
-					});
-
 					newRule = postcss.rule({ selector: fallbackSelector + ' ' + rule.selector });
 					newRule.source = rule.source;
-
 					newDecl = postcss.decl({
 						prop: 'background-image',
-						value: 'url(' + newImage + ')',
+						value: 'url(' + pngImage + ')',
 					});
 					newDecl.source = matchedBackgroundImageDecl.source;
-
 					newRule.append(newDecl);
 					rule.parent.insertAfter(rule, newRule);
 				} else {
